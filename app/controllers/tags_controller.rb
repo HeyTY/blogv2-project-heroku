b7511f487ab4ee55ac6ccf9974d71301d1ae39eb
Ty Le <anhtuan.n.le@gmail.com>
2016-11-13 18:37:57 -0500

    Updates for heroku deployment

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,24 @@
+class TagsController < ApplicationController
+	
+	def index
+	@tag = Tag.all
+	end
+
+	def show
+	@tag = Tag.find(params[:id])
+	end
+
+	def destroy
+	@tag = Tag.find(params[:id])
+	@tag.destroy
+	
+	flash.notice = "Tag '#{@tag.name}' Deleted!"
+	
+	redirect_to tags_path
+	end
+
+	before_filter :require_login, only: [:destroy]
+	
+end
+
+
